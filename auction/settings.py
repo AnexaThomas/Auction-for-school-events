f2c9dca98a48162b5687f23af6f275d2ddee9324
Anexa Thomas <anexathomasatees@gmail.com>
2025-07-11 17:19:27 +0530

    Added wildcard to ALLOWED_HOSTS

--- auction/settings.py
+++ auction/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0%=k#v+=)r7vz8x__3+7h)tkv@mlkermmm#kpxzuj6c9f0tzya
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
